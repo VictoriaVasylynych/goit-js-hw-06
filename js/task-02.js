deeecfbc63043a39e537fe6777c2d9affe7aebae
VictoriaVasylynych <solomvasylynych@gmail.com>
2021-10-19 15:58:57 +0300

    fixed task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,15 +6,56 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const itemEl = document.querySelector('#ingredients')
-console.log(itemEl);
+const listIngredients = [];
 
-const listEl = ingredients.forEach((ingredient) => {
-
-const itemListEl = document.createElement('li');
-    itemListEl.classList.add('.item');
-    itemListEl.textContent = ingredient;
-    itemEl.appendChild(itemListEl);
-    console.log(itemListEl);
-    
+ingredients.forEach((ingredient) => {
+  const itemListEl = document.createElement('li');
+  itemListEl.classList.add('.item');
+  itemListEl.textContent = ingredient;
+  listIngredients.push(itemListEl)
 });
+ 
+document.querySelector('#ingredients').append(...listIngredients);
+console.log(listIngredients)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const ingredients = [
+//  'Potatoes',
+//  'Mushrooms',
+//  'Garlic',
+//  'Tomatos',
+//  'Herbs',
+//  'Condiments',
+//];
+//
+//const ulIngredients = document.querySelector('ul#ingredients');
+//
+//const liElements = ingredients.map((ingredient) => {
+//  
+//  const elementLi = document.createElement('li');
+//  elementLi.textContent = ingredient;
+//  elementLi.classList.add('item');
+//
+//  return elementLi;
+//});
+//
+//console.log(liElements);
+//
+//ulIngredients.append(...liElements)
