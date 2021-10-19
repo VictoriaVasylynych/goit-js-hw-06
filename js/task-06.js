6e4cf2275e60f0519eb05398f943e7cf4a2ccdb3
VictoriaVasylynych <solomvasylynych@gmail.com>
2021-10-19 15:14:21 +0300

    fixed task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,12 @@
 const input = document.querySelector('#validation-input')
 console.log(input)
 
-const validInput = (event) => {
-  if (event.currentTarget.value.length === 6) {
-    event.currentTarget.classList.add("valid");
-    event.currentTarget.classList.replace("invalid", "valid");
+input.addEventListener("blur", event => {
+  if (event.currentTarget.value.length !== Number(input.dataset.length)) {
+    input.classList.remove("valid");
+    input.classList.add("invalid");
   } else {
-    event.currentTarget.classList.add("invalid");
-    event.currentTarget.classList.replace("valid", "invalid");
+    input.classList.remove("invalid");
+    input.classList.add("valid");
   }
-};
-
-input.addEventListener("blur", validInput);
+});
